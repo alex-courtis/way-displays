61b4788afae01a845f7d6fb07339d90d8108e70d
Alexander Courtis <alex@courtis.org>
2021-09-01 13:05:16 +1000

    skeletal polling loop, handle removal of heads etc.

---
+++ src/types.c
@@ -0,0 +1,63 @@
+#include "types.h"
+
+void free_mode(struct Mode *mode) {
+	/* fprintf(stderr, "free_mode %p\n", (void*)mode); */
+	if (!mode)
+		return;
+
+	free(mode);
+}
+
+void free_head(struct Head *head) {
+	/* fprintf(stderr, "free_head %p\n", (void*)head); */
+	if (!head)
+		return;
+
+	for (struct SList *i = head->modes; i; i = i->nex) {
+		free_mode(i->val);
+	}
+	slist_free(&head->modes);
+
+	free(head);
+}
+
+void free_output_manager(struct OutputManager *output_manager) {
+	/* fprintf(stderr, "free_output_manager %p\n", (void*)output_manager); */
+	if (!output_manager)
+		return;
+
+	for (struct SList *i = output_manager->heads; i; i = i->nex) {
+		free_head(i->val);
+	}
+	slist_free(&output_manager->heads);
+	slist_free(&output_manager->desired.heads_ordered);
+
+	free(output_manager);
+}
+
+void head_release_mode(struct Head *head, struct Mode *mode) {
+	if (!head || !mode)
+		return;
+
+	if (head->desired.mode == mode) {
+		head->desired.mode = NULL;
+	}
+	if (head->current_mode == mode) {
+		head->current_mode = NULL;
+	}
+
+	/* fprintf(stderr, "head_release_mode mode %dx%d\n", mode->width, mode->height); */
+
+	slist_remove(&head->modes, mode);
+}
+
+void output_manager_release_head(struct OutputManager *output_manager, struct Head *head) {
+	if (!output_manager || !head)
+		return;
+
+	/* fprintf(stderr, "output_manager_release_head head %s\n", head->name); */
+
+	slist_remove(&output_manager->desired.heads_ordered, head);
+	slist_remove(&output_manager->heads, head);
+}
+
