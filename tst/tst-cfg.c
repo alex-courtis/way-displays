dfa6b3b19fd14ab84b94f4a6d269cf2f70da516c
Alexander Courtis <alex@courtis.org>
2023-03-11 16:45:28 +1100

    cfg validate_fix validate_warn unit tests

--- tst/tst-cfg.c
+++ tst/tst-cfg.c
@@ -13,6 +13,8 @@
 // forward declarations
 struct Cfg *merge_set(struct Cfg *to, struct Cfg *from);
 struct Cfg *merge_del(struct Cfg *to, struct Cfg *from);
+void validate_warn(struct Cfg *cfg);
+void validate_fix(struct Cfg *cfg);
 
 struct UserScale *us(const char *name_desc, const float scale) {
 	struct UserScale *us = calloc(1, sizeof(struct UserScale));
@@ -70,6 +72,12 @@ int after_each(void **state) {
 
 	free(s);
 	return 0;
+}
+
+void __wrap_log_warn(const char *__restrict __format, const char *arg1, const char *arg2, ...) {
+	check_expected(__format);
+	check_expected(arg1);
+	check_expected(arg2);
 }
 
 void merge_set__arrange(void **state) {
@@ -240,6 +248,141 @@ void merge_del__disabled(void **state) {
 	cfg_free(merged);
 }
 
+void validate_fix__col(void **state) {
+	struct State *s = *state;
+
+	s->from->arrange = COL;
+	s->from->align = TOP;
+	expect_string(__wrap_log_warn, __format, "\nIgnoring invalid ALIGN %s for %s arrange. Valid values are LEFT, MIDDLE and RIGHT. Using default LEFT.");
+	expect_string(__wrap_log_warn, arg1, "TOP");
+	expect_string(__wrap_log_warn, arg2, "COLUMN");
+
+	s->expected->arrange = COL;
+	s->expected->align = LEFT;
+
+	validate_fix(s->from);
+
+	assert_equal_cfg(s->from, s->expected);
+}
+
+void validate_fix__row(void **state) {
+	struct State *s = *state;
+
+	s->from->arrange = ROW;
+	s->from->align = RIGHT;
+	expect_string(__wrap_log_warn, __format, "\nIgnoring invalid ALIGN %s for %s arrange. Valid values are TOP, MIDDLE and BOTTOM. Using default TOP.");
+	expect_string(__wrap_log_warn, arg1, "RIGHT");
+	expect_string(__wrap_log_warn, arg2, "ROW");
+
+	s->expected->arrange = ROW;
+	s->expected->align = TOP;
+
+	validate_fix(s->from);
+
+	assert_equal_cfg(s->from, s->expected);
+}
+
+void validate_fix__scale(void **state) {
+	struct State *s = *state;
+
+	char FMT[] = "\nIgnoring non-positive SCALE %s %.3f";
+
+	slist_append(&s->from->user_scales, us("ok", 1));
+
+	slist_append(&s->from->user_scales, us("neg", -1));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "neg");
+	expect_any(__wrap_log_warn, arg2);
+
+	slist_append(&s->from->user_scales, us("zero", 0));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "zero");
+	expect_any(__wrap_log_warn, arg2);
+
+	validate_fix(s->from);
+
+	slist_append(&s->expected->user_scales, us("ok", 1));
+
+	assert_equal_cfg(s->from, s->expected);
+}
+
+void validate_fix__mode(void **state) {
+	struct State *s = *state;
+
+	slist_append(&s->from->user_modes, um("ok", false, 1, 2, 3, false));
+	slist_append(&s->from->user_modes, um("max", true, -1, -1, -1, false));
+
+	slist_append(&s->from->user_modes, um("negative width", false, -99, 2, 3, false));
+	expect_string(__wrap_log_warn, __format, "\nIgnoring non-positive MODE %s WIDTH %d");
+	expect_string(__wrap_log_warn, arg1, "negative width");
+	expect_any(__wrap_log_warn, arg2);
+
+	slist_append(&s->from->user_modes, um("negative height", false, 1, -99, 3, false));
+	expect_string(__wrap_log_warn, __format, "\nIgnoring non-positive MODE %s HEIGHT %d");
+	expect_string(__wrap_log_warn, arg1, "negative height");
+	expect_any(__wrap_log_warn, arg2);
+
+	slist_append(&s->from->user_modes, um("negative hz", false, 1, 2, -99, false));
+	expect_string(__wrap_log_warn, __format, "\nIgnoring non-positive MODE %s HZ %d");
+	expect_string(__wrap_log_warn, arg1, "negative hz");
+	expect_any(__wrap_log_warn, arg2);
+
+	slist_append(&s->from->user_modes, um("missing width", false, -1, 2, 3, false));
+	expect_string(__wrap_log_warn, __format, "\nIgnoring invalid MODE %s missing WIDTH");
+	expect_string(__wrap_log_warn, arg1, "missing width");
+	expect_any(__wrap_log_warn, arg2);
+
+	slist_append(&s->from->user_modes, um("missing height", false, 1, -1, 3, false));
+	expect_string(__wrap_log_warn, __format, "\nIgnoring invalid MODE %s missing HEIGHT");
+	expect_string(__wrap_log_warn, arg1, "missing height");
+	expect_any(__wrap_log_warn, arg2);
+
+	validate_fix(s->from);
+
+	slist_append(&s->expected->user_modes, um("ok", false, 1, 2, 3, false));
+	slist_append(&s->expected->user_modes, um("max", true, -1, -1, -1, false));
+
+	assert_equal_cfg(s->from, s->expected);
+}
+
+void validate_warn__(void **state) {
+	struct State *s = *state;
+
+	char FMT[] = "\n%s '%s' is less than 4 characters, which may result in some unwanted matches.";
+
+	slist_append(&s->expected->user_scales, us("sss", 1));
+	slist_append(&s->expected->user_scales, us("ssssssss", 2));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "SCALE");
+	expect_string(__wrap_log_warn, arg2, "sss");
+
+	slist_append(&s->expected->user_modes, um("mmm", false, 1, 1, 1, false));
+	slist_append(&s->expected->user_modes, um("mmmmmmmm", false, 1, 1, 1, false));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "MODE");
+	expect_string(__wrap_log_warn, arg2, "mmm");
+
+	slist_append(&s->expected->order_name_desc, strdup("ooo"));
+	slist_append(&s->expected->order_name_desc, strdup("oooooooooo"));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "ORDER");
+	expect_string(__wrap_log_warn, arg2, "ooo");
+
+	slist_append(&s->expected->max_preferred_refresh_name_desc, strdup("ppp"));
+	slist_append(&s->expected->max_preferred_refresh_name_desc, strdup("pppppppppp"));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "MAX_PREFERRED_REFRESH");
+	expect_string(__wrap_log_warn, arg2, "ppp");
+
+	slist_append(&s->expected->disabled_name_desc, strdup("ddd"));
+	slist_append(&s->expected->disabled_name_desc, strdup("dddddddddd"));
+	expect_string(__wrap_log_warn, __format, FMT);
+	expect_string(__wrap_log_warn, arg1, "DISABLED");
+	expect_string(__wrap_log_warn, arg2, "ddd");
+
+	validate_warn(s->expected);
+}
+
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		TEST(merge_set__arrange),
@@ -253,6 +396,13 @@ int main(void) {
 		TEST(merge_del__scale),
 		TEST(merge_del__mode),
 		TEST(merge_del__disabled),
+
+		TEST(validate_fix__col),
+		TEST(validate_fix__row),
+		TEST(validate_fix__scale),
+		TEST(validate_fix__mode),
+
+		TEST(validate_warn__),
 	};
 
 	return RUN(tests);
