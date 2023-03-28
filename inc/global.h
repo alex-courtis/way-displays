952639322636f1eff33d4ddfed1575d7dde65f95
Alexander Courtis <alex@courtis.org>
2023-03-28 16:04:05 +1100

    70 test layout handle_success, extract global

---
+++ inc/global.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern struct Displ *displ;
+extern struct Cfg *cfg;
+extern struct Lid *lid;
+
+extern struct Head *head_changing_mode;
+extern struct Head *head_changing_adaptive_sync;
+
+#endif // GLOBAL_H
