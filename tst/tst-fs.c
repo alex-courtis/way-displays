4aec0f760c3430f3fd354c01ac55a64492ba4919
Alexander Courtis <alex@courtis.org>
2025-03-21 15:33:57 +1100

    mkdir_p__no_perm creates real directory to test in

--- tst/tst-fs.c
+++ tst/tst-fs.c
@@ -11,58 +11,54 @@
 
 #include "fs.h"
 
-void clean_files(void) {
-	rmdir("tst/tmp/mkdir_p/foo/bar");
-	rmdir("tst/tmp/mkdir_p/foo");
-	rmdir("tst/tmp/mkdir_p");
+void clean_dirs(void) {
+	rmdir("tst/mkdir_p/foo/bar");
+	rmdir("tst/mkdir_p/foo");
+	rmdir("tst/mkdir_p");
+
+	struct stat sb;
+	assert_int_equal(stat("tst/mkdir_p", &sb), -1);
+	assert_int_equal(errno, ENOENT);
 }
 
-
-struct State {
-	struct Cfg *from;
-	struct Cfg *to;
-	struct Cfg *expected;
-};
-
 int before_all(void **state) {
-	mkdir("tst/tmp", 0755);
+	clean_dirs();
 	return 0;
 }
 
 int after_all(void **state) {
-	rmdir("tst/tmp");
+	clean_dirs();
 	return 0;
 }
 
 int before_each(void **state) {
-	clean_files();
-
+	clean_dirs();
 	return 0;
 }
 
 int after_each(void **state) {
-	clean_files();
-
+	clean_dirs();
 	return 0;
 }
 
-// TODO this fails on CI as the test runs as root
+void mkdir_p__no_perm(void **state) {
+	assert_true(mkdir_p("tst/mkdir_p", 0755));
+	assert_true(mkdir_p("tst/mkdir_p/foo", 0555));
 
-// void mkdir_p__no_perm(void **state) {
-// 	assert_false(mkdir_p("/foo/bar", 0755));
+	assert_false(mkdir_p("tst/mkdir_p/foo/bar", 0755));
 
-// 	assert_log(ERROR, "\nCannot create directory /foo\n");
-
-// 	struct stat sb;
-// 	assert_int_equal(stat("/foo/bar", &sb), -1);
-// 	assert_int_equal(errno, ENOENT);
-// }
-
-void mkdir_p__ok(void **state) {
-	assert_true(mkdir_p("tst/tmp/mkdir_p/foo/bar", 0755));
+	assert_log(ERROR, "\nCannot create directory tst/mkdir_p/foo/bar\n");
 
 	struct stat sb;
-	assert_int_equal(stat("tst/tmp/mkdir_p/foo/bar", &sb), 0);
+	assert_int_equal(stat("tst/mkdir_p/foo/bar", &sb), -1);
+	assert_int_equal(errno, ENOENT);
+}
+
+void mkdir_p__ok(void **state) {
+	assert_true(mkdir_p("tst/mkdir_p/foo", 0755));
+
+	struct stat sb;
+	assert_int_equal(stat("tst/mkdir_p/foo", &sb), 0);
 }
 
 void mkdir_p__exists(void **state) {
@@ -70,18 +66,18 @@ void mkdir_p__exists(void **state) {
 	mode |=       S_IRGRP | S_IXGRP;
 	mode |=       S_IROTH | S_IXOTH;
 
-	assert_true(mkdir_p("tst/tmp/mkdir_p/foo/bar", mode));
+	assert_true(mkdir_p("tst/mkdir_p/foo", mode));
 
-	assert_true(mkdir_p("tst/tmp/mkdir_p/foo/bar", mode));
+	assert_true(mkdir_p("tst/mkdir_p/foo", mode));
 
 	struct stat sb;
-	assert_int_equal(stat("tst/tmp/mkdir_p/foo/bar", &sb), 0);
+	assert_int_equal(stat("tst/mkdir_p/foo", &sb), 0);
 }
 
 
 int main(void) {
 	const struct CMUnitTest tests[] = {
-		// TEST(mkdir_p__no_perm),
+		TEST(mkdir_p__no_perm),
 		TEST(mkdir_p__ok),
 		TEST(mkdir_p__exists),
 	};
