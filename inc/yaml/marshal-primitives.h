4430f10b1b9fdd6fd9b81a22418f4101f6a373fd
Alexander Courtis <alex@courtis.org>
2026-05-13 06:58:00 +1000

    libyaml migration

--- inc/yaml/marshal-primitives.h
+++ inc/yaml/marshal-primitives.h
@@ -8,33 +8,26 @@
 #include "slist.h"
 
 /*
- * Marshal primitives: false on failure
+ * Functions to add entries to an existing yaml document nodes
+ * Returns false on failure to add to the document
  */
 
-bool yaml_map_add_str(const char *k, const char *v, int mapping);
+// yaml_map_populate_fn: add a scalar to a map node
+bool yaml_map_add_str(const char *key, const char *str, int mapping);
+bool yaml_map_add_int(const char *key, const int32_t val, int mapping);
+bool yaml_map_add_float(const char *key, const float val, int mapping);
+bool yaml_map_add_bool(const char *key, const bool val, int mapping);
+bool yaml_map_add_enum(const char *key, const int val, enum_name_fn fn_name, int mapping);
 
-bool yaml_map_add_int(const char *k, const int32_t v, int mapping);
+// yaml_seq_append_fn: add a scalar to a sequence node
+bool yaml_seq_append_str(const void *str, int sequence);
 
-bool yaml_map_add_float(const char *k, const float v, int mapping);
-
-bool yaml_map_add_bool(const char *k, const bool v, int mapping);
-
-bool yaml_map_add_enum(const char *k, const int v, enum_name_fn fn_name, int mapping);
-
-/*
- * yaml_seq_append_fn: false on failure
- */
-
-bool yaml_seq_append_str(const void *data, int sequence);
-
-/*
- * Marshal nodes: false on failure
- */
-
+// Add a new map to a map node by evaluating fn on data
 typedef bool (*yaml_map_populate_fn)(const void *data, const int mapping);
-bool yaml_map_add_map(const char *k, const void *data, yaml_map_populate_fn fn, int mapping);
+bool yaml_map_add_map(const char *key, const void *data, yaml_map_populate_fn fn, int mapping);
 
+// Add a new sequence to a map node by evaluating fn on each item in list
 typedef bool (*yaml_seq_append_fn)(const void *data, const int sequence);
-bool yaml_map_add_seq(const char *k, const struct SList *list, yaml_seq_append_fn fn, int mapping);
+bool yaml_map_add_seq(const char *key, const struct SList *list, yaml_seq_append_fn fn, int mapping);
 
 #endif // YAML_MARSHAL_PRIMITIVES_H
--- src/yaml/marshal-primitives.c
+++ src/yaml/marshal-primitives.c
@@ -10,94 +10,94 @@
 #include "slist.h"
 #include "yaml-marshal.h"
 
-bool yaml_map_add_str(const char *k, const char *v, int mapping) {
-	if (!k || !v || !mapping)
+bool yaml_map_add_str(const char *key, const char *str, int mapping) {
+	if (!key || !str || !mapping)
 		return false;
 
-	int key = yaml_document_add_scalar(marshal_ctx.doc, (yaml_char_t *)YAML_DEFAULT_SCALAR_TAG, (yaml_char_t *)k, -1, YAML_PLAIN_SCALAR_STYLE);
-	int scalar = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)v, -1, YAML_PLAIN_SCALAR_STYLE);
+	int k = yaml_document_add_scalar(marshal_ctx.doc, (yaml_char_t *)YAML_DEFAULT_SCALAR_TAG, (yaml_char_t *)key, -1, YAML_PLAIN_SCALAR_STYLE);
+	int v = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)str, -1, YAML_PLAIN_SCALAR_STYLE);
 
-	return key && scalar && yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, key, scalar);
+	return k && v && yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, k, v);
 }
 
-bool yaml_map_add_int(const char *k, const int32_t v, int mapping) {
-	if (!k || !mapping)
+bool yaml_map_add_int(const char *key, const int32_t val, int mapping) {
+	if (!key || !mapping)
 		return false;
 
-	char v_str[20];
-	snprintf(v_str, 20, "%d", v);
+	char str[20];
+	snprintf(str, 20, "%d", val);
 
-	return yaml_map_add_str(k, v_str, mapping);
+	return yaml_map_add_str(key, str, mapping);
 }
 
-bool yaml_map_add_float(const char *k, const float v, int mapping) {
-	if (!k || v == 0 || !mapping)
+bool yaml_map_add_float(const char *key, const float val, int mapping) {
+	if (!key || val == 0 || !mapping)
 		return false;
 
-	char v_str[100];
-	snprintf(v_str, 100, "%g", v);
+	char str[100];
+	snprintf(str, 100, "%g", val);
 
-	return yaml_map_add_str(k, v_str, mapping);
+	return yaml_map_add_str(key, str, mapping);
 }
 
-bool yaml_map_add_bool(const char *k, const bool v, int mapping) {
-	if (!k || !mapping)
+bool yaml_map_add_bool(const char *key, const bool val, int mapping) {
+	if (!key || !mapping)
 		return false;
 
-	return yaml_map_add_str(k, (v ? "TRUE" : "FALSE"), mapping);
+	return yaml_map_add_str(key, (val ? "TRUE" : "FALSE"), mapping);
 }
 
-bool yaml_map_add_enum(const char *k, const int v, enum_name_fn fn_name, int mapping) {
-	if (!k || !fn_name || !mapping)
+bool yaml_map_add_enum(const char *key, const int val, enum_name_fn fn_name, int mapping) {
+	if (!key || !fn_name || !mapping)
 		return false;
 
-	const char *v_str = fn_name(v);
-	if (!v_str)
+	const char *str = fn_name(val);
+	if (!str)
 		return false;
 
 	// use T/F to obey schema
 	if (fn_name == on_off_name)
-		v_str = (v == ON) ? "TRUE" : "FALSE";
+		str = (val == ON) ? "TRUE" : "FALSE";
 
-	return yaml_map_add_str(k, v_str, mapping);
+	return yaml_map_add_str(key, str, mapping);
 }
 
-bool yaml_seq_append_str(const void *data, int sequence) {
-	if (!data || !sequence)
+bool yaml_seq_append_str(const void *str, int sequence) {
+	if (!str || !sequence)
 		return false;
 
-	int scalar = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)data, -1, YAML_PLAIN_SCALAR_STYLE);
+	int scalar = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)str, -1, YAML_PLAIN_SCALAR_STYLE);
 
 	return scalar && yaml_document_append_sequence_item(marshal_ctx.doc, sequence, scalar);
 }
 
-bool yaml_map_add_map(const char *k, const void *data, yaml_map_populate_fn fn, int mapping) {
-	if (!k || !fn || !mapping)
+bool yaml_map_add_map(const char *key, const void *data, yaml_map_populate_fn fn, int mapping) {
+	if (!key || !fn || !mapping)
 		return false;
 
-	int key = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)k, -1, YAML_PLAIN_SCALAR_STYLE);
+	int k = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)key, -1, YAML_PLAIN_SCALAR_STYLE);
 	int map = yaml_document_add_mapping(marshal_ctx.doc, NULL, YAML_BLOCK_MAPPING_STYLE);
 
-	if (!key || !map)
+	if (!k || !map)
 		return false;
 
 	fn(data, map);
 
-	return yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, key, map);
+	return yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, k, map);
 }
 
-bool yaml_map_add_seq(const char *k, const struct SList *list, yaml_seq_append_fn fn, int mapping) {
-	if (!k || !list || !fn || !mapping)
+bool yaml_map_add_seq(const char *key, const struct SList *list, yaml_seq_append_fn fn, int mapping) {
+	if (!key || !list || !fn || !mapping)
 		return false;
 
-	int key = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)k, -1, YAML_PLAIN_SCALAR_STYLE);
-	int sequence = yaml_document_add_sequence(marshal_ctx.doc, NULL, YAML_BLOCK_SEQUENCE_STYLE);
+	int k = yaml_document_add_scalar(marshal_ctx.doc, NULL, (yaml_char_t *)key, -1, YAML_PLAIN_SCALAR_STYLE);
+	int seq = yaml_document_add_sequence(marshal_ctx.doc, NULL, YAML_BLOCK_SEQUENCE_STYLE);
 
-	if (!key || !sequence)
+	if (!k || !seq)
 		return false;
 
 	for (const struct SList *i = list; i; i = i->nex)
-		fn(i->val, sequence);
+		fn(i->val, seq);
 
-	return yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, key, sequence);
+	return yaml_document_append_mapping_pair(marshal_ctx.doc, mapping, k, seq);
 }
