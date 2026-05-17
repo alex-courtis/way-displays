0dcd06221ea056eddbda8f4a6741f2d8f46a1db5
Alexander Courtis <alex@courtis.org>
2026-05-17 16:33:42 +1000

    libyaml migration

--- .github/workflows/packages/base/install.sh
+++ .github/workflows/packages/base/install.sh
@@ -21,5 +21,6 @@ mkdir -p lib32
 curl -L https://github.com/yaml/libyaml/releases/download/0.2.5/yaml-0.2.5.tar.gz | tar zx --directory lib32
 cd lib32/yaml-0.2.5
 CFLAGS=-m32 ./configure --prefix=/usr --libdir=/usr/lib32
+cd src
 make
 make install
