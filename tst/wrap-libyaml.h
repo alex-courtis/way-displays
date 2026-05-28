7bda135ebd6b12cf18397da61a2ec6230dc86ffa
Alexander Courtis <alex@courtis.org>
2026-05-28 10:40:51 +1000

    feat(#256): normalise all unmarshalling error messages

--- tst/wrap-libyaml.h
+++ tst/wrap-libyaml.h
@@ -3,7 +3,6 @@
 // one shots to have the function fail, returning 0, otherwise call the real
 //
 extern bool yaml_document_initialize__fail;
-extern bool yaml_document_add_mapping__fail;
 
 extern bool yaml_emitter_initialize__fail;
 extern bool yaml_emitter_open__fail;
@@ -11,7 +10,6 @@ extern bool yaml_emitter_dump__fail;
 extern bool yaml_emitter_close__fail;
 
 extern bool yaml_parser_initialize__fail;
-extern bool yaml_parser_load__fail;
 
 // reset all the above
 void reset_yaml_fails(void);
