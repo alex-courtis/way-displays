a0ab752560aadd813da9b841eac7826b2ce7738d
Alexander Courtis <alex@courtis.org>
2026-06-06 12:36:58 +1000

    chore: alex-c-lib 4

---
+++ lib/alex-c/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FROM="${HOME}/src/alex-c-lib"
+TO="lib/alex-c"
+
+for l in fn itable slist sset stable str; do
+	cp -v "${FROM}/src/${l}.c" "${TO}/src"
+	cp -v "${FROM}/inc/${l}.h" "${TO}/inc"
+	if [ -f "${FROM}/tst/assert-${l}.h" ]; then
+		cp -v "${FROM}/tst/assert-${l}.h" "${TO}/tst"
+	fi
+done
+
+for t in asserts.h expects.h tst.h util-file.h util-file.c; do
+	cp -v "${FROM}/tst/${t}" "${TO}/tst"
+done
+
