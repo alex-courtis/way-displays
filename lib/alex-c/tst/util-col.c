18928134f4fc3c8cb704a9e9d5892a3cb6649e94
Alexander Courtis <alex@courtis.org>
2026-07-08 18:26:47 +1000

    chore: alex-c-lib 4

--- lib/alex-c/tst/util-col.c
+++ lib/alex-c/tst/util-col.c
@@ -95,6 +95,33 @@ size_t pmap_put_many(const struct PMap* const map, ...) {
 	return added;
 }
 
+size_t imap_put_many(const struct IMap* const map, ... /* key, val, NULL */ ) {
+	if (!map)
+		return 0;
+
+	va_list ap;
+	va_start(ap, map);
+
+	size_t added = 0;
+
+	size_t key;
+
+	// NULL terminator is odd vararg: the key
+	while ((key = va_arg(ap, size_t)) != 0) {
+
+		// trust that a value has been passed, NULL is valid
+		const void *val = va_arg(ap, void*);
+
+		if (imap_put_free(map, key, val)) {
+			added++;
+		}
+	}
+
+	va_end(ap);
+
+	return added;
+}
+
 struct SMap {
 	const struct SMapParams params;
 	const struct PMap *pmap;
--- lib/alex-c/tst/util-col.h
+++ lib/alex-c/tst/util-col.h
@@ -4,20 +4,17 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+#include "imap.h"
 #include "pmap.h"
 #include "pset.h"
-#include "sset.h"
 #include "smap.h"
 #include "smapi.h"
 #include "smaps.h"
-// TODO
-// #include "imap.h"
+#include "sset.h"
 
 /*
  * variadic mutation, not for production use, will flag code scanners
  */
-
-// TODO test limits
 
 // add if the set does not contain each val, return number added, variadic args must be NULL terminated [equal_val, alloc_val]
 size_t pset_add_many(const struct PSet* const set, ... /* , NULL */ );
@@ -26,10 +23,12 @@ size_t pset_add_many_v(const struct PSet* const set, va_list __args);
 // add if the set does not contain each val, return number added, variadic args must be NULL terminated
 size_t sset_add_many(const struct SSet* const set, ... /* , NULL */ );
 
-
 // set key/vals, free old vals, return number overwritten, variadic key/val pairs must be terminated with a NULL key [equal_key, alloc_key, alloc_val, free_key, free_val]
 size_t pmap_put_many(const struct PMap* const map, ... /* key, val, NULL */ );
 size_t pmap_put_many_v(const struct PMap* const map, va_list __args);
+
+// set key/vals, free old vals, return number overwritten, variadic key/val pairs must be terminated with a 0 key [alloc_val, free_val]
+size_t imap_put_many(const struct IMap* const map, ... /* key, val, NULL */ );
 
 // set key/vals, free old vals, return number overwritten, variadic key/val pairs must be terminated with a NULL key [alloc_val, free_val]
 size_t smap_put_many(const struct SMap* const map, ... /* key, val, NULL */ );
