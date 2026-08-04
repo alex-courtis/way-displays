23e6f7344dcab38724429af86ae93d8ac506ac7d
Alexander Courtis <alex@courtis.org>
2026-08-04 16:12:46 +1000

    fix: simpler callback cmd checking

--- src/info/callback.c
+++ src/info/callback.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,13 +18,13 @@
 #include "yaml/unmarshal-types-v1.h"
 
 // true if the callback command is a non-empty string
-static bool callback_valid(const struct Cfg * const cfg) {
+static bool callback_cmd_valid(const struct Cfg * const cfg) {
 	return cfg && cfg->callback_cmd && strlen(cfg->callback_cmd) > 0;
 }
 
 // allows callbacks before g_cfg has been set, to cover the case of unmarshalling errors
 static void callback_with_cfg(const enum LogThreshold t, const struct Cfg * const cfg, const char * const msg1, const char * const msg2) {
-	if (!callback_valid(cfg)) {
+	if (!callback_cmd_valid(cfg)) {
 		return;
 	}
 
@@ -62,7 +63,7 @@ void callback(const enum LogThreshold t, const char * const msg1, const char * c
 }
 
 void callback_mode_fail(const enum LogThreshold t, const struct Head * const head, const struct zwlr_output_mode_v1* const zmode) {
-	if (!callback_valid(g_cfg) || !head || !zmode) {
+	if (!callback_cmd_valid(g_cfg) || !head || !zmode) {
 		return;
 	}
 
@@ -82,7 +83,7 @@ void callback_mode_fail(const enum LogThreshold t, const struct Head * const hea
 }
 
 void callback_adaptive_sync_fail(const enum LogThreshold t, const struct Head * const head) {
-	if (!callback_valid(g_cfg) || !head) {
+	if (!callback_cmd_valid(g_cfg) || !head) {
 		return;
 	}
 
